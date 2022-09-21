1f4d9c3af2766d66819ef1a21ac5aa0259c0fdb9
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-21 14:50:11 +0200

    Defining HelloFreinds.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import CallUs from '../components/Homepage/CallUs'
 import Navbar from '../components/Homepage/Navbar'
+import HelloFriends from '../components/Homepage/HelloFriends'
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <CallUs />
       <Navbar />
+      <HelloFriends />
     </Wrapper>
   );
 }
