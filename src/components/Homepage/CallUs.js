71fc0f82e526e487ab97e99c736b1473e36ada08
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-21 13:38:51 +0200

    Creating CallUs.js file

---
+++ src/components/Homepage/CallUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function CallUs() {
+    return (
+        <Wrapper>
+          
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default CallUs;
