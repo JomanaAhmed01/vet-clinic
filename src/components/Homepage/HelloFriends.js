a62cd53cd72c09f773cc6ac7a865ba8ec358cf55
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-21 14:48:54 +0200

    Creating HelloFriends.js file

---
+++ src/components/Homepage/HelloFriends.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function HelloFriends() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default HelloFriends;
