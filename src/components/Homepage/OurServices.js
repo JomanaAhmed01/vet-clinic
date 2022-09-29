1443ec29892adbe794ebeae2d36d2ff08d4781e1
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-30 00:35:07 +0200

    Creating OurServices.js file

---
+++ src/components/Homepage/OurServices.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function OurServices() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default OurServices;
