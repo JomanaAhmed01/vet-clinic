73860923412467049c5dc1e00f61a1b1f7a32456
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-10-23 09:34:14 +0200

    Creating AboutOurClinic.js file

---
+++ src/components/Homepage/AboutOurClinic.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function AboutOurClinic() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default AboutOurClinic;
