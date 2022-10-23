0add0cbe0986a66e570dc07950f445a145a49a88
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-10-23 10:33:55 +0200

    Creating KeepInTouch.js file

---
+++ src/components/Homepage/KeepInTouch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function KeepInTouch() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default KeepInTouch;
