7279f25bb46f8398f95f0e2a754a09cea763c6ac
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-10-25 12:27:01 +0200

    Defining CallUs.js file & Navbar.js file in the BookAppointmentPageCompound.js file

--- src/compounds/BookAppointmentPageCompound.js
+++ src/compounds/BookAppointmentPageCompound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
+import CallUs from '../components/BookAppointmentPage/CallUs'
+import Navbar from '../components/BookAppointmentPage/Navbar'
 
 function BookAppointmentPageCompound() {
     return (
         <Wrapper>
-
+            <CallUs />
+            <Navbar />
         </Wrapper>
     );
 }
