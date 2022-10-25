f0b855ba618d15d837c2bdd681b09cfc7c8202d3
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-10-25 12:18:44 +0200

    Creating BookAppointmentPageCompound.js file

---
+++ src/compounds/BookAppointmentPageCompound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function BookAppointmentPageCompound() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default BookAppointmentPageCompound;
